6c91b84b3db860239eeab65b78b30a3060b8a29a
lilen <lilen@10.6.148.169>
2024-09-23 12:50:32 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/versioncontrol_lilenrockwell/VersionControl_LilenRockwell.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.versioncontrol_lilenrockwell;
+
+/**
+ *
+ * @author lilen
+ */
+public class VersionControl_LilenRockwell {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
